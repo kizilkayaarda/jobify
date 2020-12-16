3750043ac015eb6df6706557305da975cbb27e17
Enes <ensmrdane@gmail.com>
2020-12-16 21:28:10 +0300

    index.html displays the posts retrieved from database
    index.html displays the posts retrieved from database

--- scripts/job_postings.js
+++ scripts/job_postings.js
@@ -1,69 +1,69 @@
-var job_postings = {
-    "1": {
-        "title": "Bilgisayar Mühendisi",
-        "company": "KARACASU TEKSTİL TİCARET VE SAN.ANONİM ŞİRKETİ",
-        "city": "Kahramanmaraş",
-        "definition": "Sektöründe öncü firmamızın AR-GE tesisinde görevlendirmek üzere bilgisayar mühendisliği bölümlerinin birinden mezun olan  iyi derecede ingilizce bilen mezuniyet not ortalaması 3 ve üzeri olan çalışma arkadaşı aramaktayız ",
+var job_posting_data = [
+    {
+        "title": "Bilgisayar Muhendisi",
+        "company": "KARACASU TEKSTIL TICARET VE SAN.ANONIM SIRKETI",
+        "city": "Kahramanmaras",
+        "definition": "Sektorunde oncu firmamizin AR-GE tesisinde gorevlendirmek uzere bilgisayar muhendisligi bolumlerinin birinden mezun olan  iyi derecede ingilizce bilen mezuniyet not ortalamasi 3 ve uzeri olan calisma arkadasi aramaktayiz ",
         "website": "kariyer.net",
         "link": "https://www.kariyer.net/is-ilani/karacasu-tekstil-ticaret-ve-san-anonim-sirketi-bilgisayar-muhendisi-2615288"
     },
-    "2": {
-        "title": "Bilgisayar Mühendisi",
-        "company": "ERMETAL OTOMATİV VEEŞYA SANAYİ TİCARET A.Ş.",
+    {
+        "title": "Bilgisayar Muhendisi",
+        "company": "ERMETAL OTOMATIV VEEsYA SANAYI TICARET A.s.",
         "city": "Bursa(Osmangazi)",
-        "definition": "ERMETAL ŞİRKETLER GRUBU BT&BS MÜDÜRLÜĞÜNDE İSTİHDAM EDİLMEK ÜZERE; Üniversitelerin Bilgisayar Mühendisliği veya Bilgisayar Programcılığı bölümlerinden mezun, ERP sistemleri üzerinde yazılım geliştirme tecrübesine sahip (rapor ve yeni süreç geliştirme) Tercihen MS Dynamics NAV ERP Sistemleri üzerinde yazılım geliştirme tecrübesine sahip, MS SQL Server veri tabanı yönetim sistemlerini kullanabilen, Analitik düşünme ve problem çözme yeteneğine sahip, ERP Sorumlusu pozisyonunda çalışacak Bilgisayar Mühendisi çalışma arkadaşı aramaktayız.",
+        "definition": "ERMETAL sIRKETLER GRUBU BT&BS MuDuRLuguNDE ISTIHDAM EDILMEK uZERE; universitelerin Bilgisayar Muhendisligi veya Bilgisayar Programciligi bolumlerinden mezun, ERP sistemleri uzerinde yazilim gelistirme tecrubesine sahip (rapor ve yeni surec gelistirme) Tercihen MS Dynamics NAV ERP Sistemleri uzerinde yazilim gelistirme tecrubesine sahip, MS SQL Server veri tabani yonetim sistemlerini kullanabilen, Analitik dusunme ve problem cozme yetenegine sahip, ERP Sorumlusu pozisyonunda calisacak Bilgisayar Muhendisi calisma arkadasi aramaktayiz.",
         "website": "kariyer.net",
         "link": "https://www.kariyer.net/is-ilani/ermetal-otomotiv-veesya-sanayi-ticaret-a-s-bilgisayar-muhendisi-2633227"
     },
-    "3": {
-        "title": "Ön Muhasebe Elemanı",
-        "company": "Tatarlı İnşaat ve Seramik San. ve Tic. A.Ş.",
-        "city": "İstanbul(Avr.)(Eyüp)",
-        "definition": "*ALİBEYKÖYDE YENİ AÇILACAK MAĞAZAMIZA *TERCİHEN YÜKSEKOKUL MEZUNU *EN AZ 3 YILLIK DENEYİMİ OLAN *MİCRO ETA MUHASEBE PROGRAMLARINI BİLEN *MS OFFİCE PROGRAMLARINI İYİ DÜZEYDE BİLEN *ÖĞRENMEYİ VE GELİŞMEYİ KENDİNE İLKE EDİNEN *ÇALIŞMA RKADAŞLARI ARAMAKTAYIZ.",
+    {
+        "title": "on Muhasebe Elemani",
+        "company": "Tatarli Insaat ve Seramik San. ve Tic. A.s.",
+        "city": "Istanbul(Avr.)(Eyup)",
+        "definition": "*ALIBEYKoYDE YENI AcILACAK MAgAZAMIZA *TERCIHEN YuKSEKOKUL MEZUNU *EN AZ 3 YILLIK DENEYIMI OLAN *MICRO ETA MUHASEBE PROGRAMLARINI BILEN *MS OFFICE PROGRAMLARINI IYI DuZEYDE BILEN *ogRENMEYI VE GELIsMEYI KENDINE ILKE EDINEN *cALIsMA RKADAsLARI ARAMAKTAYIZ.",
         "website": "kariyer.net",
         "link": "https://www.kariyer.net/is-ilani/tatarli-insaat-ve-seramik-san-ve-tic-a-s-on-muhasebe-elemani-2637605"
     },
-    "4": {
-        "title": "Wordpress ve İçerik Uzmanı",
-        "company": "ÇAĞ LOJİSTİK A.Ş",
-        "city": "İstanbul(Asya)",
-        "definition": "WordPress tema ve uygulamaları geliştirmek, en az 10 tane mobil uyumlu Wordpress tabanlı web sitesi kuruculuğu yapmış olmak Yabancı dillerdeki web sitesi içeriklerini yönetebilmek Dijital dünyadaki trendleri yakından takip etmek Kullanıcı deneyimi alanındaki gelişmeleri takip etmek, mevcut web sitelerinde buna dair geliştirmeler yapabilmek İçerik üretimi ve SEO konusunda bilgili, farklı sektörler özelinde web sitesi içerikleri hazırlayabilecek Google araçlarına hakim, Photoshop ve/veya benzeri grafik-tasarım programlarını kullanabilen 'Wordpress ve İçerik Uzmanı' arayışımız bulunmaktadır",
+    {
+        "title": "Wordpress ve Icerik Uzmani",
+        "company": "cAg LOJISTIK A.s",
+        "city": "Istanbul(Asya)",
+        "definition": "WordPress tema ve uygulamalari gelistirmek, en az 10 tane mobil uyumlu Wordpress tabanli web sitesi kuruculugu yapmis olmak Yabanci dillerdeki web sitesi iceriklerini yonetebilmek Dijital dunyadaki trendleri yakindan takip etmek Kullanici deneyimi alanindaki gelismeleri takip etmek, mevcut web sitelerinde buna dair gelistirmeler yapabilmek Icerik uretimi ve SEO konusunda bilgili, farkli sektorler ozelinde web sitesi icerikleri hazirlayabilecek Google araclarina hakim, Photoshop ve/veya benzeri grafik-tasarim programlarini kullanabilen 'Wordpress ve Icerik Uzmani' arayisimiz bulunmaktadir",
         "website": "kariyer.net",
         "link": "https://www.kariyer.net/is-ilani/cag-lojistik-a-s-wordpress-ve-icerik-uzmani-2637606"
     },
-    "5": {
+    {
         "title": "Servis Komisi",
-        "company": "D.ream (Doğuş Restaurant Entertainment&Management)",
-        "city": "İstanbul(Avr.), İstanbul(Asya)",
-        "definition":"d.ream (Doğuş Restaurant Entertainment and Management) faaliyetlerine Mart 2012'de başlamış ve global olarak faaliyet gösterdiği restoran çeşitliliği ve sayısı ile F&B sektöründe lider bir konuma ulaşmıştır. Doğuş Grubu'nun desteğiyle d.ream, dünya çapında yarattığı yenilikçi yeme içme ve eğlence konseptleri ile hızla Türkiye'de pazar lideri ve uluslararası bir oyuncu haline geldi. Güçlü büyümesiyle d.ream, bugün itibariyle toplam 12 ülkede 140 restoran, 45 marka ve yaklaşık 7.000 çalışanı ile sektörde hizmet vermektedir. d.ream bünyesinde Servis Komisi pozisyonunda değerlendirilmek üzere takım arkadaşları arıyoruz. Tercihen Turizm Meslek Yüksek Okulu veya Turizm Meslek Lisesi mezunu  Tercihen restoran servis ve bar deneyimine sahip,  Tercihen iyi derecede İngilizce iletişim yeteneğine sahip İnsan ilişkilerinde başarılı, müşteri memnuniyetine önem veren  Dış görünüşüne özen gösteren, diksiyonu düzgün, dinamik  Ekip çalışmasına önem veren  Yeme içme sektöründe kariyer hedefleyen  Yoğun iş temposuna, vardiya sistemine ve esnek çalışma saatlerine uyum sağlayabilecek",
+        "company": "D.ream (Dogus Restaurant Entertainment&Management)",
+        "city": "Istanbul(Avr.), Istanbul(Asya)",
+        "definition":"d.ream (Dogus Restaurant Entertainment and Management) faaliyetlerine Mart 2012'de baslamis ve global olarak faaliyet gosterdigi restoran cesitliligi ve sayisi ile F&B sektorunde lider bir konuma ulasmistir. Dogus Grubu'nun destegiyle d.ream, dunya capinda yarattigi yenilikci yeme icme ve eglence konseptleri ile hizla Turkiye'de pazar lideri ve uluslararasi bir oyuncu haline geldi. Guclu buyumesiyle d.ream, bugun itibariyle toplam 12 ulkede 140 restoran, 45 marka ve yaklasik 7.000 calisani ile sektorde hizmet vermektedir. d.ream bunyesinde Servis Komisi pozisyonunda degerlendirilmek uzere takim arkadaslari ariyoruz. Tercihen Turizm Meslek Yuksek Okulu veya Turizm Meslek Lisesi mezunu  Tercihen restoran servis ve bar deneyimine sahip,  Tercihen iyi derecede Ingilizce iletisim yetenegine sahip Insan iliskilerinde basarili, musteri memnuniyetine onem veren  Dis gorunusune ozen gosteren, diksiyonu duzgun, dinamik  Ekip calismasina onem veren  Yeme icme sektorunde kariyer hedefleyen  Yogun is temposuna, vardiya sistemine ve esnek calisma saatlerine uyum saglayabilecek",
         "website": "kariyer.net",
         "link": "https://www.kariyer.net/is-ilani/d-ream-dogus-restaurant-entertainment-management-servis-komisi-2637597"
     },
-    "6": {
-        "title": "Bilgisayar Mühendisi",
-        "company": "Ic Yazılım Donanım Danışmanlık",
-        "city": "İzmir",
-        "definition": "Teknik tasarımı ve dökümantasyonu yapılan yazılımların  kendi platformumuz üzerinde geliştirilmesine katkı sağlamak, Platform üzerinde Rapor dizayn çalışmaları yapmak, Geliştirilen yazılımların yazılım geliştirme içi ön testlerin (birim ve entegrasyon testleri) yapılmasını sağlamak, Yapılan testler sonucu çıkan hata ve değişikliklerin giderilmesini sağlamak, Belirlenen standartlarda teknik dökümantasyon oluşturmak, Belli aşamalarda analiz için yazılım ekibi ile ortak çalışma yapmak, Uygunsuzluk durumlarında gerekli düzeltici faaliyetleri başlatmak veya başlatılmasını sağlayarak sonucunu takip edebilmek, Son kullanıcı eğitim ve desteğinde bulunmak üzere Bilgisayar Mühendisi arayışımız bulunmaktadır.",
+    {
+        "title": "Bilgisayar Muhendisi",
+        "company": "Ic Yazilim Donanim Danismanlik",
+        "city": "Izmir",
+        "definition": "Teknik tasarimi ve dokumantasyonu yapilan yazilimlarin  kendi platformumuz uzerinde gelistirilmesine katki saglamak, Platform uzerinde Rapor dizayn calismalari yapmak, Gelistirilen yazilimlarin yazilim gelistirme ici on testlerin (birim ve entegrasyon testleri) yapilmasini saglamak, Yapilan testler sonucu cikan hata ve degisikliklerin giderilmesini saglamak, Belirlenen standartlarda teknik dokumantasyon olusturmak, Belli asamalarda analiz icin yazilim ekibi ile ortak calisma yapmak, Uygunsuzluk durumlarinda gerekli duzeltici faaliyetleri baslatmak veya baslatilmasini saglayarak sonucunu takip edebilmek, Son kullanici egitim ve desteginde bulunmak uzere Bilgisayar Muhendisi arayisimiz bulunmaktadir.",
         "website": "isbul.net",
         "link": "https://www.isbul.net/ic-yazilim-donanim-danismanlik/bilgisayar-muhendisi-is-ilani-210646"
     },
-    "7": {
+    {
         "title": "Yeni Mezun Avukat",
-        "company": "Skamar Gayrimenkul A.Ş",
-        "city": "İstanbul",
-        "definition": "Gayrimenkul yatırım şirketi için hukuk departmanımıza, Danışman Avukatlarımıza yardımcı olmak ve hukuksal takip işlerini yapmak üzere tam zamanlı çalışacak yeni mezun Avukat aranmaktadır.",
+        "company": "Skamar Gayrimenkul A.s",
+        "city": "Istanbul",
+        "definition": "Gayrimenkul yatirim sirketi icin hukuk departmanimiza, Danisman Avukatlarimiza yardimci olmak ve hukuksal takip islerini yapmak uzere tam zamanli calisacak yeni mezun Avukat aranmaktadir.",
         "website": "isbul.net",
         "link": "https://www.isbul.net/skamar-gayrimenkul-a-s/yeni-mezun-avukat-is-ilani-243409"
     },
-    "8": {
+    {
         "title": "Software Engineer-Payments",
         "company": "Udemy",
-        "city": "Ankara, Ankara, Türkiye",
+        "city": "Ankara, Ankara, Turkiye",
         "definition":"Engineers in the Payments team are dedicated to implement strategic billing and payment operations to support hundreds of transactions per minute globally. Our system supports hundreds of different payment methods globally, state of the art integration capabilities with top payment vendors (Stripe, PayPal, Adyen), full-stack responsible for all the purchase flow, scaled to payout thousands of instructors monthly, and always refactored to lead the way. The team plays a mission critical role in Udemy to ensure all the monetization capabilities are highly resilient and always provide the high quality experience.",
         "website": "linkedin",
         "link": "https://www.linkedin.com/jobs/view/2286348122/?alternateChannel=search&refId=TzX3vd2DXFgW81iQEgjLmg%3D%3D&trackingId=dz7RK61ZiWPXQksVd6pvMA%3D%3D&trk=flagship3_jobs_discovery_jymbii"
     },
-    "9": {
+    {
         "title":"DevOps Engineer",
         "company": "Jotform",
         "city": "Ankara",
@@ -71,39 +71,39 @@ var job_postings = {
         "website": "linkedin",
         "link": "https://www.linkedin.com/jobs/view/2344217469/?alternateChannel=search&refId=TzX3vd2DXFgW81iQEgjLmg%3D%3D&trackingId=rtuPYE4pkL5wuZIfgl7mQg%3D%3D&trk=flagship3_jobs_discovery_jymbii"
     },
-    "10": {
-        "title": "Proje Yürütücüsü",
+    {
+        "title": "Proje Yurutucusu",
         "company": "Asarge",
         "city": "Ankara",
-        "definition": "Araştırma Danışmanlık ve Yazılım Sektöründe Faaliyet Gösteren Firmamızda İyi derecede bilgisayar kullanabilen İnternetten araştırmalar yapabilecek Online toplantılar yürütebilecek Yönetim ve iletişim becerileri yüksek Ekip kuracak ve yönetebilecek Araştırma, veri analizi, dijitalleşme konularına meraklı Gündemi takip eden ve araştırmacı yapıya sahip İş disiplini ve önceliği iş olan Yönetici Asistanı Aranmaktadır",
+        "definition": "Arastirma Danismanlik ve Yazilim Sektorunde Faaliyet Gosteren Firmamizda Iyi derecede bilgisayar kullanabilen Internetten arastirmalar yapabilecek Online toplantilar yurutebilecek Yonetim ve iletisim becerileri yuksek Ekip kuracak ve yonetebilecek Arastirma, veri analizi, dijitallesme konularina merakli Gundemi takip eden ve arastirmaci yapiya sahip Is disiplini ve onceligi is olan Yonetici Asistani Aranmaktadir",
         "website": "indeed",
         "link": "https://tr.indeed.com/viewjob?cmp=Asarge&t=Proje+Y%C3%BCr%C3%BCt%C3%BCc%C3%BCs%C3%BC&jk=2ee843070b93d8dd&q=Bilgisayar+M%C3%BChendisi&vjs=3"
     },
-    "11": {
-        "title": "Gömülü Yazılım Tasarım Mühendisi",
+    {
+        "title": "Gomulu Yazilim Tasarim Muhendisi",
         "company": "KAREL ELEKTRONIK SANAYI VE TICARET A.S.",
         "city": "Ankara",
-        "definition": "GENEL NİTELİKLER VE İŞ TANIMI Üniversitelerin Elektrik/Elektronik veya Bilgisayar Mühendisliği bölümünden mezun, En az 3 yıl C ve C++ programlama dili ile kodlama, birim test, hata giderme, doğrulama faaliyetleri yapmış, en az bir proje tamamlamış, Arm, DSP işlemciler, mikro denetleyiciler ve çevre birimlerinde tecrübe sahibi, Tercihen algoritma geliştirme ve/veya işletim sistemi tecrübesi olan, İyi derecede İngilizce bilen ve İngilizce doküman hazırlayabilecek, Yazılım geliştirme modelleri hakkında bilgi sahibi, Erkek adaylar için askerlik hizmetini tamamlamış, Analitik düşünme, sorun çözme, yeni fikirler üretme yeteneğine sahip, ekip çalışmasına yatkın, çalışma arkadaşı aramaktayız.",
+        "definition": "GENEL NITELIKLER VE Is TANIMI universitelerin Elektrik/Elektronik veya Bilgisayar Muhendisligi bolumunden mezun, En az 3 yil C ve C++ programlama dili ile kodlama, birim test, hata giderme, dogrulama faaliyetleri yapmis, en az bir proje tamamlamis, Arm, DSP islemciler, mikro denetleyiciler ve cevre birimlerinde tecrube sahibi, Tercihen algoritma gelistirme ve/veya isletim sistemi tecrubesi olan, Iyi derecede Ingilizce bilen ve Ingilizce dokuman hazirlayabilecek, Yazilim gelistirme modelleri hakkinda bilgi sahibi, Erkek adaylar icin askerlik hizmetini tamamlamis, Analitik dusunme, sorun cozme, yeni fikirler uretme yetenegine sahip, ekip calismasina yatkin, calisma arkadasi aramaktayiz.",
         "website": "careerjet",
         "link": "https://www.careerjet.com.tr/jobad/tr562a877fc51490f430974cdb368ebe5a"
     },
-    "12": {
-        "title": "Sinyalizasyon Sistem Mühendisi",
-        "company": "TMS TREN BAKIM ONARIM A.Ş",
+    {
+        "title": "Sinyalizasyon Sistem Muhendisi",
+        "company": "TMS TREN BAKIM ONARIM A.s",
         "city": "Ankara",
-        "definition": "ETCS araç üstü bakım prosedürlerini uygulamak ve denetlemek, İşin teknik altyapısına hakim olmak için Türkçe ve İngilizce doküman incelemek, Ekip liderleri ile saha çalışmalarını planlamak ve organize etmek, -Yurtiçi - yurtdışı çalışma ortaklarımızla iletişim ve koordinasyon içinde olmak, - aha ve yönetim arasında bilgi alışverişini sağlamak, Arıza durumunda arızanın kaynağını belirlemek, Trenlerin zamanında servise hazır hale gelmesini sağlamak.",
+        "definition": "ETCS arac ustu bakim prosedurlerini uygulamak ve denetlemek, Isin teknik altyapisina hakim olmak icin Turkce ve Ingilizce dokuman incelemek, Ekip liderleri ile saha calismalarini planlamak ve organize etmek, -Yurtici - yurtdisi calisma ortaklarimizla iletisim ve koordinasyon icinde olmak, - aha ve yonetim arasinda bilgi alisverisini saglamak, Ariza durumunda arizanin kaynagini belirlemek, Trenlerin zamaninda servise hazir hale gelmesini saglamak.",
         "website": "indeed",
         "link": "https://tr.indeed.com/viewjob?jk=94cd57945c17c9f2&tk=1epk0eda3stuk800&from=serp&vjs=3"
     },
-    "13": {
-        "title": "Php Yazılımcı",
-        "company": "Kariyer Adam Eğitim Yazılım",
+    {
+        "title": "Php Yazilimci",
+        "company": "Kariyer Adam Egitim Yazilim",
         "city": "Ankara",
-        "definition": "PHP tabanlı web yazılım projelerimizin geliştirilmesi ve mevcut yazılımların güncellenmesi çalışmalarında yer alacak,Ankara Kızılaydaki firmamızda tam zamanlı çalışabilecek, PHP Yazılım Uzmanı çalışma arkadaşı aramaktayız.",
+        "definition": "PHP tabanli web yazilim projelerimizin gelistirilmesi ve mevcut yazilimlarin guncellenmesi calismalarinda yer alacak,Ankara Kizilaydaki firmamizda tam zamanli calisabilecek, PHP Yazilim Uzmani calisma arkadasi aramaktayiz.",
         "website": "eleman.net",
         "link": "https://www.eleman.net/is-ilani/php-yazilimci-i2934777"
     },
-    "14": {
+    {
         "title": "Unity Developer",
         "company": "Deplike",
         "city": "Ankara",
@@ -111,12 +111,12 @@ var job_postings = {
         "website": "linkedin",
         "link": "https://www.linkedin.com/jobs/view/2326267650/?alternateChannel=search&refId=TzX3vd2DXFgW81iQEgjLmg%3D%3D&trackingId=TxIWkTG1v0fQmhVLk932XA%3D%3D"
     },
-    "15": {
+    {
         "title": "Backend Developer",
         "company": "VLMedia",
         "city": "Ankara",
         "definition": "We are looking for passionate candidates to join our young and energetic team. You will be part of our Development Team to generate systems for more than 500 Million daily client requests. You will find, save, and change data and serve it back to users.",
         "website": "linkedin",
         "link": "https://www.linkedin.com/jobs/view/2309444855/?alternateChannel=search&refId=TzX3vd2DXFgW81iQEgjLmg%3D%3D&trackingId=8R589JVM3A4xOf%2BoZH%2B%2B%2Fg%3D%3D"
-    }    
-}
+    }
+]
