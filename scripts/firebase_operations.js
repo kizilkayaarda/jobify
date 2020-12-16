840b358a13c17a7e5336922e060c3599159cac10
Enes <ensmrdane@gmail.com>
2020-12-17 02:10:18 +0300

    firebase_create_job_offer method is created in firebase_operations.js
    firebase_create_job_offer method is created in firebase_operations.js

--- scripts/firebase_operations.js
+++ scripts/firebase_operations.js
@@ -69,3 +69,15 @@ function firebase_signOut(){
       });
 } 
 
+function firebase_create_job_offer(dataUpload){
+    firebase.firestore().collection('jobOffers').doc("offersList").get().then((data) => {
+        let finalData = data.data().job_posting_data;
+
+        finalData.push(dataUpload)
+
+        firebase.firestore().collection('jobOffers').doc("offersList").set({finalData})
+        .then({
+            
+        })
+    })
+}
